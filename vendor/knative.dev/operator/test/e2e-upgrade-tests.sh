8c92d65776b959776cb822a3cfd814218fbb7dba
Vincent Hou <shou@us.ibm.com>
2022-03-16 11:42:29 -0400

    [WIP] Add the integration tests

---
+++ vendor/knative.dev/operator/test/e2e-upgrade-tests.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 The Knative Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script runs the end-to-end tests against Knative Serving
+# Operator built from source. However, this script will download the latest
+# source code of knative serving and generated the latest manifest file for
+# serving installation. The serving operator will use this newly generated
+# manifest file to replace the one under the directory
+# cmd/manager/kodata/knative-serving. This purpose of this script is to verify
+# whether the latest source code of operator can work properly with the latest
+# source code of knative serving.
+
+# If you already have a Knative cluster setup and kubectl pointing
+# to it, call this script with the --run-tests arguments and it will use
+# the cluster and run the tests.
+
+# Calling this script without arguments will create a new cluster in
+# project $PROJECT_ID, start knative in it, run the tests and delete the
+# cluster.
+
+export GO111MODULE=auto
+
+source "$(dirname "${BASH_SOURCE[0]}")/e2e-common.sh"
+
+# Skip installing istio as an add-on.
+initialize "$@" --skip-istio-addon
+
+TIMEOUT=30m
+
+header "Running upgrade tests"
+
+go_test_e2e -tags=upgrade -timeout=${TIMEOUT} \
+  ./test/upgrade \
+  --preservingversion="${PREVIOUS_SERVING_RELEASE_VERSION}" --preeventingversion="${PREVIOUS_EVENTING_RELEASE_VERSION}" \
+  || fail_test
+
+success

a4ba17dcd2b698fae5153de237cf088b58d4ca66
Vincent <shou@us.ibm.com>
2022-07-08 15:57:09 -0400

    Add integration tests for the removal of env vars (#182)

---
+++ test/e2e/eventingenvvardeletion_test.go
@@ -0,0 +1,83 @@
+//go:build eventingenvvarsremove
+// +build eventingenvvarsremove
+
+/*
+Copyright 2022 The Knative Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"testing"
+
+	"knative.dev/kn-plugin-operator/pkg/command/configure"
+	"knative.dev/kn-plugin-operator/test/resources"
+	"knative.dev/operator/test"
+	"knative.dev/operator/test/client"
+)
+
+// TestEventingEnvvarDeletion verifies whether the operator plugin can delete the env vars of the deployment for Knative Eventing
+func TestEventingEnvvarDeletion(t *testing.T) {
+	clients := client.Setup(t)
+
+	names := test.ResourceNames{
+		KnativeServing:  "knative-serving",
+		KnativeEventing: "knative-eventing",
+		Namespace:       resources.EventingOperatorNamespace,
+	}
+
+	test.CleanupOnInterrupt(func() { test.TearDown(clients, names) })
+	defer test.TearDown(clients, names)
+
+	for _, tt := range []struct {
+		name                string
+		expectedEnvVarFlags configure.EnvVarFlags
+	}{{
+		name: "Knative Eventing verifying the env var deletion for the deployment",
+		expectedEnvVarFlags: configure.EnvVarFlags{
+			EnvName:       resources.TestEnvName,
+			Component:     "eventing",
+			Namespace:     resources.EventingOperatorNamespace,
+			DeployName:    "eventing-controller",
+			ContainerName: "eventing-controller",
+			EnvValue:      resources.TestEnvValue,
+		},
+	}, {
+		name: "Knative Eventing verifying the additional env var deletion for the deployment",
+		expectedEnvVarFlags: configure.EnvVarFlags{
+			EnvName:       resources.TestAddEnvName,
+			Component:     "eventing",
+			Namespace:     resources.EventingOperatorNamespace,
+			DeployName:    "eventing-controller",
+			ContainerName: "eventing-controller",
+			EnvValue:      resources.TestAddEnvValue,
+		},
+	}, {
+		name: "Knative Eventing verifying the env var deletion for the deployment activator",
+		expectedEnvVarFlags: configure.EnvVarFlags{
+			EnvName:       resources.TestEnvName,
+			Component:     "eventing",
+			Namespace:     resources.EventingOperatorNamespace,
+			DeployName:    "eventing-webhook",
+			ContainerName: "eventing-webhook",
+			EnvValue:      resources.TestEnvValue,
+		},
+	}} {
+		t.Run(tt.name, func(t *testing.T) {
+			resources.VerifyKnativeEventingEnvVarsDeletion(t, clients.Operator.KnativeEventings(resources.EventingOperatorNamespace),
+				tt.expectedEnvVarFlags)
+		})
+	}
+}

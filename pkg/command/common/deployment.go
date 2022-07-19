e3dabdad83efbf148434405d20bb4d0f5dc2da5b
Vincent Hou <shou@us.ibm.com>
2022-07-19 11:46:52 -0400

    Check whether the existing knative

--- pkg/command/common/deployment.go
+++ pkg/command/common/deployment.go
@@ -19,13 +19,18 @@ package common
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 )
 
-const KnativeOperatorName = "knative-operator"
+const (
+	KnativeOperatorName       = "knative-operator"
+	KnativeServingActivator   = "activator"
+	KnativeEventingController = "eventing-controller"
+)
 
 // Deployment is used to access the cluster to check if the deployment of the knative operator exists
 type Deployment struct {
@@ -33,24 +38,49 @@ type Deployment struct {
 }
 
 // CheckIfOperatorInstalled checks if Knative Operator exists
-func (d *Deployment) CheckIfOperatorInstalled() (bool, error) {
+func (d *Deployment) CheckIfOperatorInstalled() (bool, string, string, error) {
+	return d.CheckIfKeyDeploymentInstalled(KnativeOperatorName)
+}
+
+// CheckIfOperatorInstalled checks if Knative Component exists
+func (d *Deployment) CheckIfKnativeInstalled(component string) (bool, string, string, error) {
+	if strings.EqualFold(component, ServingComponent) {
+		return d.CheckIfKnativeServingInstalled()
+	}
+	return d.CheckIfKnativeEventingInstalled()
+}
+
+func (d *Deployment) CheckIfKeyDeploymentInstalled(name string) (bool, string, string, error) {
 	namespaces, err := d.Client.CoreV1().Namespaces().List(context.TODO(), metav1.ListOptions{})
 	if err != nil {
-		return false, fmt.Errorf("failed to receive a namespace list: %s", err)
+		return false, "", "", fmt.Errorf("failed to receive a namespace list: %s", err)
 	}
 
 	found := false
+	namespace := ""
+	version := ""
 	for _, ns := range namespaces.Items {
-		// Create if the Knative Operator deployment exists
-		_, err := d.Client.AppsV1().Deployments(ns.ObjectMeta.Name).Get(context.TODO(), KnativeOperatorName, metav1.GetOptions{})
+		deploy, err := d.Client.AppsV1().Deployments(ns.ObjectMeta.Name).Get(context.TODO(), name, metav1.GetOptions{})
 		if apierrors.IsNotFound(err) {
 			continue
 		} else if err != nil {
-			return false, err
+			return false, "", "", err
 		}
 		found = true
+		namespace = ns.ObjectMeta.Name
+		if value, ok := deploy.ObjectMeta.Labels["app.kubernetes.io/version"]; ok {
+			version = value
+		}
 		break
 	}
 
-	return found, nil
+	return found, namespace, version, nil
+}
+
+func (d *Deployment) CheckIfKnativeServingInstalled() (bool, string, string, error) {
+	return d.CheckIfKeyDeploymentInstalled(KnativeServingActivator)
+}
+
+func (d *Deployment) CheckIfKnativeEventingInstalled() (bool, string, string, error) {
+	return d.CheckIfKeyDeploymentInstalled(KnativeEventingController)
 }

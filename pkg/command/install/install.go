a0023cbe39e590d1e2acd7094f24e06f6a4a3fea
Vincent Hou <shou@us.ibm.com>
2022-07-22 11:32:16 -0400

    Implement the upgrade across multiple versions

--- pkg/command/install/install.go
+++ pkg/command/install/install.go
@@ -15,17 +15,28 @@
 package install
 
 import (
+	"context"
 	"fmt"
 	"math"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/spf13/cobra"
 	"golang.org/x/mod/semver"
+	v1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/kubernetes"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/oidc" // from https://github.com/kubernetes/client-go/issues/345
+
 	"knative.dev/kn-plugin-operator/pkg"
 	"knative.dev/kn-plugin-operator/pkg/command/common"
+	"knative.dev/operator/pkg/apis/operator/v1beta1"
+	operatorv1beta1 "knative.dev/operator/pkg/client/clientset/versioned/typed/operator/v1beta1"
+	"knative.dev/pkg/test/logging"
 )
 
 type installCmdFlags struct {
@@ -35,6 +46,17 @@ type installCmdFlags struct {
 	KubeConfig     string
 	Version        string
 }
+
+var (
+	ServingKeyDeployments = []string{"activator", "autoscaler", "autoscaler-hpa", "controller", "domain-mapping",
+		"domainmapping-webhook", "webhook"}
+	EventingKeyDeployments = []string{"eventing-controller", "eventing-webhook", "imc-controller", "imc-dispatcher",
+		"mt-broker-controller", "mt-broker-filter", "mt-broker-ingress", "pingsource-mt-adapter"}
+	// Interval specifies the time between two polls.
+	Interval = 10 * time.Second
+	// Timeout specifies the timeout for the function PollImmediate to reach a certain status.
+	Timeout = 5 * time.Minute
+)
 
 func (flags *installCmdFlags) fill_defaults() {
 	if flags.Version == "" {
@@ -108,13 +130,21 @@ func RunInstallationCommand(installFlags *installCmdFlags, p *pkg.OperatorParams
 		return err
 	}
 
+	client, err := p.NewKubeClient()
+	if err != nil {
+		return fmt.Errorf("cannot get source cluster kube config, please use --kubeconfig or export environment variable KUBECONFIG to set\n")
+	}
+	deploy := common.Deployment{
+		Client: client,
+	}
+
 	if installFlags.Component != "" {
 		currentVersion := ""
-		if exists, ns, version, err := checkIfKnativeInstalled(p, installFlags.Component); err != nil {
+		if exists, ns, version, err := deploy.CheckIfKnativeInstalled(installFlags.Component); err != nil {
 			return err
 		} else if exists {
 			// Check if the namespace is consistent
-			if strings.EqualFold(ns, installFlags.Namespace) {
+			if !strings.EqualFold(ns, installFlags.Namespace) {
 				return fmt.Errorf("The namespace %s you specified is not consistent with the existing namespace for Knative Component %s",
 					installFlags.Namespace, ns)
 			}
@@ -239,18 +269,6 @@ func getYamlValuesContent(installFlags *installCmdFlags) string {
 	return content
 }
 
-func checkIfKnativeInstalled(p *pkg.OperatorParams, component string) (bool, string, string, error) {
-	client, err := p.NewKubeClient()
-	if err != nil {
-		return false, "", "", fmt.Errorf("cannot get source cluster kube config, please use --kubeconfig or export environment variable KUBECONFIG to set\n")
-	}
-	deploy := common.Deployment{
-		Client: client,
-	}
-
-	return deploy.CheckIfKnativeInstalled(component)
-}
-
 func checkIfOperatorInstalled(p *pkg.OperatorParams) (bool, string, string, error) {
 	client, err := p.NewKubeClient()
 	if err != nil {
@@ -294,6 +312,50 @@ func installKnativeComponent(installFlags *installCmdFlags, rootPath string, p *
 	}
 
 	// Make sure all the deployment resources are up and running
+	err = ensureKnativeComponentReady(installFlags, p)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ensureKnativeComponentReady(installFlags *installCmdFlags, p *pkg.OperatorParams) error {
+	client, err := p.NewKubeClient()
+	if err != nil {
+		return fmt.Errorf("cannot get source cluster kube config, please use --kubeconfig or export environment variable KUBECONFIG to set\n")
+	}
+
+	operatorClient, err := p.NewOperatorClient()
+	if err != nil {
+		return fmt.Errorf("cannot get source cluster kube config, please use --kubeconfig or export environment variable KUBECONFIG to set\n")
+	}
+
+	if strings.EqualFold(installFlags.Component, common.ServingComponent) {
+		err := WaitForKnativeDeploymentState(client, installFlags.Namespace, installFlags.Version, ServingKeyDeployments,
+			IsKnativeDeploymentReady)
+		if err != nil {
+			return err
+		}
+		_, err = WaitForKnativeServingState(operatorClient.OperatorV1beta1().KnativeServings(installFlags.Namespace), common.KnativeServingName,
+			installFlags.Version, IsKnativeServingReady)
+
+		if err != nil {
+			return err
+		}
+	} else if strings.EqualFold(installFlags.Component, common.EventingComponent) {
+		err := WaitForKnativeDeploymentState(client, installFlags.Namespace, installFlags.Version, EventingKeyDeployments,
+			IsKnativeDeploymentReady)
+		if err != nil {
+			return err
+		}
+		_, err = WaitForKnativeEventingState(operatorClient.OperatorV1beta1().KnativeEventings(installFlags.Namespace), common.KnativeEventingName,
+			installFlags.Version, IsKnativeEventingReady)
+
+		if err != nil {
+			return err
+		}
+	}
 
 	return nil
 }
@@ -357,6 +419,18 @@ func applyOverlayValuesOnTemplate(yamlTemplateString string, installFlags *insta
 
 func generateVersionStages(source, target string) ([]string, error) {
 	stringArray := ""
+
+	if strings.HasPrefix(source, "v") {
+		source = source[1:]
+	}
+	if strings.HasPrefix(target, "v") {
+		target = target[1:]
+	}
+	if source == "" {
+		stringArray = target
+		return strings.Split(stringArray, ","), nil
+	}
+
 	targetVersion := target
 	if targetVersion == common.Latest || targetVersion == common.Nightly {
 		targetVersion = common.LatestVersion
@@ -371,8 +445,9 @@ func generateVersionStages(source, target string) ([]string, error) {
 		sourceVersion = fmt.Sprintf("v%s", sourceVersion)
 	}
 
-	sourceMajor := strings.Split(sourceVersion, ".")[0]
-	targetMajor := strings.Split(targetVersion, ".")[0]
+	sourceMajor := strings.Split(sourceVersion, ".")[0][1:]
+	targetMajor := strings.Split(targetVersion, ".")[0][1:]
+
 	if targetMajor != sourceMajor {
 		return nil, fmt.Errorf("Unable to migrate from the source version %s to the target version %s", sourceVersion,
 			targetVersion)
@@ -386,7 +461,7 @@ func generateVersionStages(source, target string) ([]string, error) {
 		return nil, fmt.Errorf("minor number of the target version %v should be an integer", targetVersion)
 	}
 	if math.Abs(float64(targetMinor-sourceMinor)) < 2 {
-		stringArray = targetVersion
+		stringArray = target
 		return strings.Split(stringArray, ","), nil
 	}
 
@@ -398,7 +473,7 @@ func generateVersionStages(source, target string) ([]string, error) {
 			if minor != targetMinor {
 				stringArray = stringArray + "," + targetMajor + "." + fmt.Sprintf("%d", minor) + ".0"
 			} else {
-				stringArray = stringArray + "," + targetVersion
+				stringArray = stringArray + "," + target
 			}
 		}
 		return strings.Split(stringArray, ","), nil
@@ -411,9 +486,145 @@ func generateVersionStages(source, target string) ([]string, error) {
 			if minor != targetMinor {
 				stringArray = stringArray + "," + targetMajor + "." + fmt.Sprintf("%d", minor) + ".0"
 			} else {
-				stringArray = stringArray + "," + targetVersion
+				stringArray = stringArray + "," + target
 			}
 		}
 		return strings.Split(stringArray, ","), nil
 	}
+}
+
+// WaitForKnativeDeploymentState polls the status of the Knative deployments every `interval`
+// until `inState` returns `true` indicating the deployments match the desired deployments.
+func WaitForKnativeDeploymentState(client kubernetes.Interface, namespace string, version string, expectedDeployments []string,
+	inState func(deps *v1.DeploymentList, expectedDeployments []string, version string, err error) (bool, error)) error {
+	span := logging.GetEmitableSpan(context.Background(), fmt.Sprintf("WaitForKnativeDeploymentState/%s/%s", expectedDeployments, "KnativeDeploymentIsReady"))
+	defer span.End()
+
+	waitErr := wait.PollImmediate(Interval, Timeout, func() (bool, error) {
+		dpList, err := client.AppsV1().Deployments(namespace).List(context.TODO(), metav1.ListOptions{})
+		return inState(dpList, expectedDeployments, version, err)
+	})
+
+	return waitErr
+}
+
+// IsKnativeDeploymentReady will check the status conditions of the deployments and return true if the deployments meet the desired status.
+func IsKnativeDeploymentReady(dpList *v1.DeploymentList, expectedDeployments []string, version string, err error) (bool, error) {
+	if err != nil {
+		return false, err
+	}
+
+	findDeployment := func(name string, deployments []v1.Deployment) *v1.Deployment {
+		for _, deployment := range deployments {
+			if deployment.Name == name {
+				return &deployment
+			}
+		}
+		return nil
+	}
+
+	isStatusReady := func(status v1.DeploymentStatus) bool {
+		for _, c := range status.Conditions {
+			if c.Type == v1.DeploymentAvailable && c.Status == corev1.ConditionTrue {
+				return true
+			}
+		}
+		return false
+	}
+
+	isReady := func(d *v1.Deployment) bool {
+		for key, val := range d.GetObjectMeta().GetLabels() {
+			// Check if the version matches. As long as we find a value equals to the version, we can determine
+			// the deployment is for the specific version. The key "networking.knative.dev/ingress-provider" is
+			// used to indicate the network ingress resource.
+			// Currently, the network ingress resource is still specified together with the knative serving.
+			// It is possible that network ingress resource is not using the same version as knative serving.
+			// This is the reason why we skip the version checking for network ingress resource.
+
+			// The parameter version means the target version of Knative component to be installed.
+			// The parameter existingVersion means the installed version of Knative component. It is set to empty, if
+			// there is no Knative installation.
+
+			// If the deployment resource is for ingress, we will check the status of the deployment.
+			if key == "networking.knative.dev/ingress-provider" {
+				return isStatusReady(d.Status)
+			}
+
+			if key == "app.kubernetes.io/version" || key == "serving.knative.dev/release" || key == "eventing.knative.dev/release" {
+				if val == fmt.Sprintf("v%s", version) || val == version {
+					// When on of the following conditions is met:
+					// * spec.version is set to latest, but operator returns an actual semantic version
+					// * spec.version is set to a valid semantic version
+					// we need to verify the value of the key serving.knative.dev/release or eventing.knative.dev/release
+					// matches the version.
+					return isStatusReady(d.Status)
+				}
+			}
+		}
+		return false
+	}
+
+	for _, name := range expectedDeployments {
+		dep := findDeployment(name, dpList.Items)
+		if dep == nil {
+			return false, nil
+		}
+		if !isReady(dep) {
+			return false, nil
+		}
+	}
+
+	return true, nil
+}
+
+// WaitForKnativeServingState polls the status of the KnativeServing called name
+// from client every `interval` until `inState` returns `true` indicating it
+// is done, returns an error or timeout.
+func WaitForKnativeServingState(clients operatorv1beta1.KnativeServingInterface, name string, version string,
+	inState func(s *v1beta1.KnativeServing, version string, err error) (bool, error)) (*v1beta1.KnativeServing, error) {
+	span := logging.GetEmitableSpan(context.Background(), fmt.Sprintf("WaitForKnativeServingState/%s/%s", name, "KnativeServingIsReady"))
+	defer span.End()
+
+	var lastState *v1beta1.KnativeServing
+	waitErr := wait.PollImmediate(Interval, Timeout, func() (bool, error) {
+		state, err := clients.Get(context.TODO(), name, metav1.GetOptions{})
+		lastState = state
+		return inState(lastState, version, err)
+	})
+
+	if waitErr != nil {
+		return lastState, fmt.Errorf("knativeserving %s is not in desired state, got: %+v: %w", name, lastState, waitErr)
+	}
+	return lastState, nil
+}
+
+// IsKnativeServingReady will check the status conditions of the KnativeServing and return true if the KnativeServing is ready.
+func IsKnativeServingReady(s *v1beta1.KnativeServing, version string, err error) (bool, error) {
+	return s.Status.IsReady() && version == s.Status.Version, err
+}
+
+// WaitForKnativeEventingState polls the status of the KnativeEventing called name
+// from client every `interval` until `inState` returns `true` indicating it
+// is done, returns an error or timeout.
+func WaitForKnativeEventingState(clients operatorv1beta1.KnativeEventingInterface, name string, version string,
+	inState func(s *v1beta1.KnativeEventing, version string, err error) (bool, error)) (*v1beta1.KnativeEventing, error) {
+	span := logging.GetEmitableSpan(context.Background(), fmt.Sprintf("WaitForKnativeEventingState/%s/%s", name, "KnativeEventingIsReady"))
+	defer span.End()
+
+	var lastState *v1beta1.KnativeEventing
+	waitErr := wait.PollImmediate(Interval, Timeout, func() (bool, error) {
+		state, err := clients.Get(context.TODO(), name, metav1.GetOptions{})
+		lastState = state
+		return inState(lastState, version, err)
+	})
+
+	if waitErr != nil {
+		return lastState, fmt.Errorf("KnativeEventing %s is not in desired state, got: %+v: %w", name, lastState, waitErr)
+	}
+	return lastState, nil
+}
+
+// IsKnativeEventingReady will check the status conditions of the KnativeEventing and return true if the KnativeEventing is ready.
+func IsKnativeEventingReady(s *v1beta1.KnativeEventing, version string, err error) (bool, error) {
+	return s.Status.IsReady() && version == s.Status.Version, err
 }
